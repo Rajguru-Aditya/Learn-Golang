f4e6f43bcfc34b2595118221eb3a3b2bbd943220
Aditya Rajguru <77228474+Rajguru-Aditya@users.noreply.github.com>
2023-01-20 15:50:00 +0530

    Creating APIs

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -35,6 +36,41 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
+	fmt.Println("Welcome to APIs with Golang")
+
+	r := mux.NewRouter()
+
+	// Seeding
+	courses = append(courses, Course{
+		CourseId:    "2",
+		CourseName:  "Golang",
+		CoursePrice: 100,
+		Author: &Author{
+			Fullname: "Aditya Rajguru",
+			Website:  "learncodeonline.dev",
+		},
+	})
+
+	courses = append(courses, Course{
+		CourseId:    "6",
+		CourseName:  "ReactJS",
+		CoursePrice: 299,
+		Author: &Author{
+			Fullname: "Aditya Rajguru",
+			Website:  "learncodeonline.dev",
+		},
+	})
+
+	// Routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers - file
@@ -88,6 +124,15 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// TODO: check if course already exists
+	// loop, title matches with course.coursename, JSON
+	for _, c := range courses {
+		if c.CourseName == course.CourseName {
+			json.NewEncoder(w).Encode("Course already exists")
+			return
+		}
+	}
+
 	// generate unique id, string
 	// append course into courses
 
@@ -119,4 +164,28 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	// TODO send a response when id is not found
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// first - grab id from request
+	params := mux.Vars(r)
+
+	// second - loop through courses and find one with the id from the request
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode("Course with id: " + params["id"] + " has been deleted")
+}
+func deleteAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete all courses")
+	w.Header().Set("Content-Type", "application/json")
+	courses = []Course{}
+	json.NewEncoder(w).Encode("All courses have been deleted")
 }
